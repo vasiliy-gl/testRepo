96463f9eb3f4131b6f4ce55cc1318ad9630cad8a
vasiliy-gl <git.learn@yandex.ru>
2017-11-07 16:38:32 +0400

    first lambda

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,5 +1,6 @@
 public class MyClass {
     public static void main(String[] args) {
         System.out.println("Hi!");
+        new Thread(()-> System.out.println("My first lambda")).start();
     }
 }
