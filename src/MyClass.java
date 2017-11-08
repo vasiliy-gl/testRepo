c8db85600293fbca4e53719936218e0970461a71
vasiliy-gl <git.learn@yandex.ru>
2017-11-09 00:30:17 +0400

    first lambda

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,13 +1,24 @@
+import java.beans.Expression;
+
 public class MyClass {
+    Expression expression;
+
     public static void main(String[] args) {
         System.out.println("Hi!");
         new Thread(MyClass::myPrint).start();
-        Runnable r = ()->MyClass.myPrint();
+        OneMethod method = MyClass::myPrint;
+        Runnable r = method::doIt;
+        new Thread(r).start();
     }
 
 
 
     static void myPrint(){
         System.out.println("Method reference");
+    }
+
+    @FunctionalInterface
+    public interface OneMethod{
+        void doIt();
     }
 }
